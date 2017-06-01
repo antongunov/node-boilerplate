c87131aedd6f5423c60213b00c5fce64445cd292
Anton Gunov <anton@gunov.name>
2017-06-01 23:48:48 +0700

    Support for environment variables

--- server/app.js
+++ server/app.js
@@ -6,8 +6,12 @@
 
 const http = require('http');
 
+const NODE_PORT = parseInt(process.env.NODE_PORT, 10);
+
 let srv = http.createServer((req, res) => {
     res.end('Hi!');
 });
 
-srv.listen(5000);
+srv.listen(NODE_PORT, () => {
+    console.log(`Server listening on port ${NODE_PORT} in ${process.env.NODE_ENV} mode...`);
+});
