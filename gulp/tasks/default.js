03fa46bc54baeb6de8d52e8fe6b5d6216f55c743
Anton Gunov <anton@gunov.name>
2017-06-04 22:01:13 +0700

    Added gulp tasks directory and default task

---
+++ gulp/tasks/default.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const gulp = require('gulp');
+
+require('./pug');
+
+gulp.task('default', [ 'pug' ]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,3 @@
 'use strict';
 
-const gulp = require('gulp');
-const pug = require('gulp-pug');
-
-gulp.task('pug', () => {
-    return gulp.src('server/pages/*.pug')
-        .pipe(pug())
-        .pipe(gulp.dest('build'));
-});
+require('./gulp/tasks/default');
