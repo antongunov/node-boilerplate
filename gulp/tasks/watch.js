07e0f5ce88cafbde4f58717b5c0454a66daccc32
Anton Gunov <anton@gunov.name>
2017-06-09 19:42:35 +0700

    Adding page template

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,7 +7,7 @@ const gulp = require('gulp');
  */
 
 gulp.task('watch', (done) => {
-    gulp.watch('server/pages/*.pug', gulp.series('pug'));
+    gulp.watch([ 'server/pages/*.pug', 'server/pages/assets/pug/**/*.pug' ], gulp.series('pug'));
     gulp.watch('server/pages/*.!(pug)', gulp.series('static'));
     gulp.watch('server/pages/assets/sass/**/*.scss', gulp.series('sass'));
     return done();
