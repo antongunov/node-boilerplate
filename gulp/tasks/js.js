52550ec7cb55bd043af750ad4930ba8ba8bc2189
Anton Gunov <anton@gunov.name>
2017-06-20 19:08:11 +0700

    Support for Babel

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,12 +1,33 @@
 'use strict';
 
 const gulp = require('gulp');
+const PluginError = require('gulp-util').PluginError;
+
+/**
+ * Gulp plugins
+ */
+
+const babel = require("gulp-babel");
+
+/**
+ * Log errors nicely
+ */
+
+babel.logError = (end) => {
+    return (err) => {
+        const message = new PluginError('babel', err).toString();
+        process.stderr.write(`${message}\n`);
+        return end();
+    };
+};
 
 /**
  * Gulp tasks
  */
 
-gulp.task('js', () => {
+gulp.task('js', (done) => {
     return gulp.src('server/pages/assets/js/app.js')
+        .pipe(babel())
+        .on('error', babel.logError(done))
         .pipe(gulp.dest('build/assets/js/'));
 });
