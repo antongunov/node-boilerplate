674e0114e690c9f70e8d551edbd964671ee4944b
Anton Gunov <anton@gunov.name>
2017-07-03 16:52:00 +0700

    Updating API router

--- server/api/index.js
+++ server/api/index.js
@@ -4,4 +4,17 @@ router.get('/ping', (req, res) => {
   res.status(200).json('PONG!');
 });
 
+router.use((req, res) => {
+  res.status(404).json({
+    error: `Unknown API endpoint "${req.method} ${req.url}"`,
+  });
+});
+
+router.use((err, req, res, next) => {
+  /* eslint no-unused-vars: off */
+  res.status(500).json({
+    error: 'General API error',
+  });
+});
+
 module.exports = router;
